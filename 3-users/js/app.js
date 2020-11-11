550fc944e91c0155fa08a0679ad0ce686a44871e
agapoli89 <agapoli@gmail.com>
2020-11-11 10:09:28 +0100

    fetch practise

--- 3-users/js/app.js
+++ 3-users/js/app.js
@@ -1,7 +1,9 @@
 const usersList = document.querySelector('#usersList')
 const template = document.querySelector('#templateElement');
+const templatePost = document.querySelector('#templatePost');
 
 
+//get users and generate their elements:
 fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => {
         if (response.ok) {
@@ -11,8 +13,6 @@ fetch("https://jsonplaceholder.typicode.com/users")
         }
     })
     .then(response => {
-        console.log(response); 
-        
         response.forEach(element => {
             const clone = template.content.cloneNode(true);
 
@@ -27,15 +27,59 @@ fetch("https://jsonplaceholder.typicode.com/users")
             userEmail.setAttribute('href', `mailto:${element.email}`)
             usersList.append(clone);   
         });
+            const postsButtons = document.querySelectorAll('.user-show-posts');
+    
+                postsButtons.forEach(postsButton => {  
+                    postsButton.addEventListener('click', showPosts)
+                });
 
-        
-        
     })
     .catch(error => {
         console.log(error)
     });
 
+    
+    const showPosts = (e) => {
+        const btn = e.path[0];
+        console.log(btn);
+        const userID = e.path[1].dataset.id;
+        fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userID}`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error (`Http error ${response.status}`);
+            }
+        })
+        .then(response => {
+            console.log(response);
+            btn.removeEventListener('click', showPosts);
+            const ulPosts = btn.nextElementSibling;
 
+            response.forEach(element => {
+                const clone = templatePost.content.cloneNode(true);
+                const postTitle = clone.querySelector('.post-title');
+                const postBody = clone.querySelector('.post-body');
+                postTitle.textContent = element.title;
+                postBody.textContent = element.body;
+                ulPosts.append(clone);       
+                })
+            togglePosts(btn, ulPosts);
+            btn.addEventListener('click', function(){
+                togglePosts(btn, ulPosts);
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    };
 
-
-
+    const togglePosts = (el, ulPosts) => {
+        if (el.innerText === "Show posts") {
+            el.textContent = 'Hide posts';
+            ulPosts.style.display = "block"; 
+        } else {
+            el.textContent = 'Show posts';
+            ulPosts.style.display = "none"; 
+        }
+    }
